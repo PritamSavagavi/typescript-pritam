a7c978e64e8486d30b03301cb71c5efc7f5f8d86
PritamSavagavi <pritamsavagavi@gmail.com>
2020-11-27 08:39:46 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-function add(n1:number, n2:number)
-{
-  return n1+n2;
+function add(n1: number, n2: number,strings:string) {
+  return strings +" "+ n1 + n2;
 }
 
-const str = "pritam savagavi"
-const number1 = 123;
-const number2 = 382;
-const result = add(number1, number2);
-console.log(str + result);
+const str = "pritam savagavi";
+const number1 = 3;
+const number2 = 4;
+const result = add(number1, number2, str);
+console.log(result);
